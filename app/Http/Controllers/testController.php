962537e5b300fa663eeef1d4cb6ceb44106fc4f1
GolamSakib <sakib.pust.ice@gmail.com>
2024-06-25 18:45:02 +0600

    alkalkkl

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class testController extends Controller
 {
-    public function index(){
-        $filepath=database_path('data/shopify_products.json');
-        dd(file_get_contents($filepath));
+    public function index()
+    {
+        $filepath = database_path('data/shopify_products.json');
+        $total = collect(File::json($filepath)['products'])->filter(fn($product) => $product['product_type'] == 'Jacket' || $product['product_type'] == 'Coat')->map(fn($item) => $item['variants'])->flatten(1)->reduce(function ($total, $item) {
+            return $total += floatval($item['price']);
+        }, 0);
+        dd($total);
+
     }
 }
